e7503dbf249c80ebcaa9eb66555a78b87dd5450c
Erwin <erwin@gti.co.id>
2019-03-14 08:56:46 +0700

    change

--- App.js
+++ App.js
@@ -21,38 +21,48 @@ export default class App extends Component {
 
   constructor() {
     super()
-    this.buttonPressed = this.buttonPressed.bind(this)
+    this.state = {}
   }
 
-  buttonPressed(){
-   
-   console.log(this.state.username, this.state.password)
-  }
-
- 
  
   render() {
     return (
       <View style={styles.container}>
-      
-      <Text>Username</Text>
-        <TextInput 
-          defaultValue = {this.state.username}
-          onChangeText = {text => this.setState({username: text})}
-        />
-
-        <Text>Password</Text>
-        <TextInput 
-          defaultValue = {this.state.password}
-          onChangeText = {text => this.setState({password: text})}
-        />
-
-        <Button
-          title="Learn More"
-          onPress={this.buttonPressed}
-          color="#841584"
-        />
-
+       <View style={styles.result}>
+          <Text style={styles.resultText}>11*11</Text>
+       </View>
+       <View style={styles.calculation}>
+          <Text style{styles.calculationText}>111</Text>
+       </View>
+        <View style={styles.buttons}>
+          <View style={styles.numbers}></View>
+             <View style={styles.row}>
+                 <Button title="0" />
+                 <Button title="0" />
+                 <Button title="0" />
+               </View> 
+              <View style={styles.row}>
+                 <Button title="0" />
+                 <Button title="0" />
+                 <Button title="0" />
+              </View>
+             <View style={styles.row}>
+                 <Button title="0" />
+                 <Button title="0" />
+                 <Button title="0" />
+             </View>
+             <View style={styles.row}>
+                <Button title="0" />
+                <Button title="0" />
+                <Button title="0" />
+             </View>
+          <View style={styles.operations}>
+           <Button title="+" />
+           <Button title="+" />
+           <Button title="+" />
+           <Button title="+" />
+          </View>
+        </View>
       </View>
     );
   }
@@ -61,7 +71,44 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    padding: 20
   },
+  calculationText:{
+    fontSize: 20,
+    color: 'white',
+  },
+  resultText:{
+    fontSize: 24,
+    color: 'white', 
+  },
+  row:{
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'stretch'
+  }
+  result: {
+    flex: 1,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'flex-end',  
+  },
+  calculation: {
+    flex: 1,
+    backgroundColor: 'green'
+  },
+  buttons: {
+    flexGrow: 1,
+    flexDirection: 'row'
+  },
+  numbers: {
+    flex: 3,
+    backgroundColor: 'yellow'
+  },
+  operations: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
+    backgroundColor: 'black'
+  },
+
 });
